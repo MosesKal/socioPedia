b18e9cafe3d3f420aab9dc9e5d773e5e92175dc1
MosesK <mosesziongo@gmail.com>
2023-08-02 22:35:51 +0200

    creation de quelque routes et controlleurs

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import {register} from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import {register} from "./controllers/auth.js";     
 
 /**CONFIGURATIONS */
 
@@ -39,8 +41,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /**ROUTES WITH */
-
 app.post("/auth/register", upload.single("picture"), register);
+
+/** ROUTES */
+app.use("/auth", authRoutes);
 
 /** MONGOOSE SETUP  */
 
